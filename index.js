dcae1245447ad2df382255bbe3a4bcff918d024e
Anjan MK <nikitha.dc@gmail.com>
2020-12-13 16:32:09 -0500

    final

--- index.js
+++ index.js
@@ -20,47 +20,52 @@ if (myArgs[1] == "--output=./") {
   });
 }
 
-// For employee collection
-fs.createReadStream("./data.csv")
-  .pipe(csv())
-  .on("data", (data) => {
-    count++;
-    for (var key in data) {
-      data[key.trim()] = data[key].trim();
-    }
-
-    var employee = new Employee({
-      EmployeeID: data["EmployeeID"],
-      Fname: data["First Name"],
-      Lname: data["Last Name"],
-      Phone: data["Phone Number"],
-      Email: data["Email"],
-    });
-
-    //save in database
-    employee.save(function (err, savedObj) {
-      if (err) {
-        console.log(
-          "Line Number " +
-            count +
-            " There is an error in processing employee data: " +
-            err
-        );
-      } else {
-        console.log("Employee data has been saved");
-
-        if (myArgs[1] == "--output=./") {
-          savedObj = savedObj.toObject();
-          let csvOutput = `${savedObj["EmployeeID"]},${savedObj["Fname"]},${savedObj["Lname"]},${savedObj["Phone"]},${savedObj["Email"]},${savedObj["createdAt"]},${savedObj["updatedAt"]}`;
-          fs.appendFile("./output.csv", csvOutput + "\n", (err) => {
-            if (err) {
-              console.log(err);
-            }
-          });
+if ((myArgs.length >0 ) && (myArgs[0].indexOf("input") >= 0)){
+    // For employee collection
+    fs.createReadStream("./data.csv")
+      .pipe(csv())
+      .on("data", (data) => {
+        count++;
+        for (var key in data) {
+          data[key.trim()] = data[key].trim();
         }
-      }
-    });
-  })
-  .on("end", () => {
-    console.log("Done");
-  });
+
+        var employee = new Employee({
+          EmployeeID: data["EmployeeID"],
+          Fname: data["First Name"],
+          Lname: data["Last Name"],
+          Phone: data["Phone Number"],
+          Email: data["Email"],
+        });
+
+        //save in database
+        employee.save(function (err, savedObj) {
+          if (err) {
+            console.log(
+              "Line Number " +
+                count +
+                " There is an error in processing employee data: " +
+                err
+            );
+          } else {
+            console.log("Employee data has been saved");
+
+            if (myArgs[1] == "--output=./") {
+              savedObj = savedObj.toObject();
+              let csvOutput = `${savedObj["EmployeeID"]},${savedObj["Fname"]},${savedObj["Lname"]},${savedObj["Phone"]},${savedObj["Email"]},${savedObj["createdAt"]},${savedObj["updatedAt"]}`;
+              fs.appendFile("./output.csv", csvOutput + "\n", (err) => {
+                if (err) {
+                  console.log(err);
+                }
+              });
+            }
+          }
+        });
+      })
+      .on("end", () => {
+        console.log("Done");
+      })
+  } else {
+    console.log("Please provide input parameter")
+} 
+
